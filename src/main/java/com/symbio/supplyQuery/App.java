b3e931a3dfa9cf378183f7fc335adcc8901e39c9
wuhui@igola.com <wuhui@igola.com>
2016-05-30 13:16:56 +0800

    main class

---
+++ src/main/java/com/symbio/supplyQuery/App.java
@@ -0,0 +1,10 @@
+package com.symbio.supplyQuery;
+
+/**
+ * @author Zhang Wuhui on 5/30/16
+ */
+public class App {
+    public static void main(String... args){
+        System.out.println("System starting");
+    }
+}
